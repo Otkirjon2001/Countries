b2f06cec0355a13f64b31388fa4dab779d2f530e
Otkirjon2001 <otkirsalimboyev@gmail.com>
2021-12-10 16:41:46 +0500

    Add other styles

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-const elSiteDark = document.querySelector(".body");
 const elSiteButton = document.querySelector(".header-button");
 elSiteButton.addEventListener("click", function () {
-  elSiteDark.classList.toggle("dark")
+  document.body.classList.toggle("dark")
 });
